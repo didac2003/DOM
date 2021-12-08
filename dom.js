b8319e88c0d9815132bb919acf040f39ab7132d1
didac2003 <didac.gros@institutvidreres.cat>
2021-12-08 16:46:41 +0100

    ex3

--- dom.js
+++ dom.js
@@ -4,7 +4,13 @@ let nodeMajus = document.getElementById('majuscula')
 nodeMajus.innerHTML = majuscula.innerHTML.toUpperCase()
 */
 
-//Exx2
+//Ex2
 /*
 document.getElementById("link").textContent = document.URL;
 */
+
+//Exx3
+function canviarColor(){
+    document.getElementById("groc").style.color = "yellow"
+}
+canviarColor()
