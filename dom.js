439a5344cd9902e4832c2ebfc2b0b7532dfb3110
didac2003 <94047560+didac2003@users.noreply.github.com>
2021-12-08 16:32:09 +0100

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,10 @@
+//Ex1
+/*
+let nodeMajus = document.getElementById('majuscula')
+nodeMajus.innerHTML = majuscula.innerHTML.toUpperCase()
+*/
+
+//Ex2
+/*
+document.getElementById("link").textContent = document.URL;
+*/
