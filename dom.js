9d5d2b5d6f64f20db5bfcbc5ef9e98891aebe4dd
didac2003 <didac.gros@institutvidreres.cat>
2021-12-08 16:34:37 +0100

    ex2

--- dom.js
+++ dom.js
@@ -4,7 +4,7 @@ let nodeMajus = document.getElementById('majuscula')
 nodeMajus.innerHTML = majuscula.innerHTML.toUpperCase()
 */
 
-//Ex2
+//Exx2
 /*
 document.getElementById("link").textContent = document.URL;
 */
