9e8858fc564ca0b3a84fc2d97c20ffa0966cd8bd
didac2003 <didac.gros@institutvidreres.cat>
2021-12-10 14:15:52 +0100

    ex1

--- dom.js
+++ dom.js
@@ -1,7 +1,7 @@
 //Ex1
 /*
-let nodeMajus = document.getElementById('majuscula')
-nodeMajus.innerHTML = majuscula.innerHTML.toUpperCase()
+let majus = document.getElementById('majuscula')
+majus.innerHTML = majuscula.innerHTML.toUpperCase()
 */
 
 //Ex2
@@ -18,9 +18,27 @@ canviarColor()
 */
 
 //Ex4
+/*
 function mida() {
 document.getElementById("canviarMida").width = 250
 document.getElementById("canviarMida").height = 250
 }
 mida()
+*/
+
+//Ex5
+
+/*var etiqueta = document.getElementById(String, 'llista');
+
+function toUpper(str){
+    return str
+        .toLowerCase()
+        .split(' ')
+        .map(function(Word){
+            return Word[0].fontsize(10) + Word.susbtr(1);
+        })
+        .join(' ');
+}
+document.write(toUpper('hola com anem'));*/
+    
 
