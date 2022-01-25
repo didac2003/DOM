717a9b60127f072df8755bf5674d706db4747f2c
didac2003 <didac.gros@institutvidreres.cat>
2022-01-25 23:46:32 +0100

    hol

---
+++ ex5.js
@@ -0,0 +1,22 @@
+// let llista = document.querySelectorAll(".llista")
+
+// llista.forEach(list =>{
+//     let hol = list.textContent.slice(0,1)
+//     list.innerHTML=hol
+// })
+
+// let paraa = document.getElementById("paragraf").textContent
+
+// let dividit = paraa.split(" ")
+
+// console.log(dividit.length);
+
+// let foto = document.createElement("img")
+
+// foto.src="../Projecte/adidas1.jpg"
+
+// document.getElementById("fotaka").appendChild(foto)
+
+// let boto = document.getElementById("boto").value
+
+// document.getElementById("div").innerHTML = boto
